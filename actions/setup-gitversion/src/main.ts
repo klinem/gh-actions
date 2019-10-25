d0ce1806e96de0214d704a43a018d89a0a7a80ba
Kline Moralee <klinemoralee@gmail.com>
2019-10-25 20:15:25 +1100

    Export gitversion variables

--- actions/setup-gitversion/src/main.ts
+++ actions/setup-gitversion/src/main.ts
@@ -1,4 +1,6 @@
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+
 import { GitVersionInstaller } from './installer';
 
 async function run() {
@@ -7,6 +9,22 @@ async function run() {
 
     const installer = new GitVersionInstaller(version);
     await installer.install();
+
+    let output = '';
+    exec.exec('dotnet-gitversion', ['/output', 'json'], {
+      listeners: {
+        stdout: (data: Buffer) => {
+          output += data.toString();
+        },
+      },
+    });
+
+    core.info(output);
+
+    const gitVersionResult: { [key: string]: string } = JSON.parse(output);
+    for (const key in gitVersionResult) {
+      core.exportVariable(`GITVERSION_${key}`, gitVersionResult[key]);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
